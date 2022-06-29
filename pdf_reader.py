75f71f36300bf37c7ce6da4105543667adf70b06
Amadeo <domingofal@gmail.com>
2022-06-29 00:23:12 -0300

    Implementation of pdf parser and vis

---
+++ pdf_reader.py
@@ -0,0 +1,143 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+from PyPDF2 import PdfReader
+
+
+def create_form(parent):
+    first_row = {
+    'Nombre' : [None, None],
+    'SBA' : [None, None, 10]
+}
+    second_row = {
+    'Sexo' : [None, None, 9],
+    'Pelaje' : [None, None],
+    'Nacimiento' : [None, None, 11],
+    'RP' : [None, None, 6]
+}
+    third_row = {
+    'Criador' : [None, None],
+    'Propietario' : [None, None]
+}
+    fourth_row = {
+    'Padre' : [None, None],
+    'RP' : [None, None, 6],
+    'SBA' : [None, None, 10]
+}
+    fifth_row = {
+    'Madre' : [None, None],
+    'RP' : [None, None, 6],
+    'SBA' : [None, None, 10]
+}
+    widget_holder = [first_row, second_row, third_row, fourth_row, fifth_row]
+
+    for row in widget_holder:
+        new_frame = ttk.Frame(parent)
+        new_frame.pack(fill='x', padx=5, pady=5)
+        for index, (key, value) in enumerate(row.items()):
+            value[0] = ttk.Label(new_frame, text= key + ' : ')
+            if index == 0:
+                value[0]['width'] = 9
+            value[1] = ttk.Entry(new_frame)
+            value[0].pack(side='left', padx= (5,0), pady=5)
+            if len(value) == 3:
+                value[1]['width'] = value[2]
+                value[1]['justify'] = 'right'
+                if key == 'Sexo':
+                    value[1]['justify'] = 'left'
+                elif key == 'Nacimiento':
+                    value[1]['justify'] = 'center'
+                value[1].pack(side='left', padx= (0,5), pady=5)
+            else:
+                value[1].pack(side='left', expand= 1, fill='x', padx= (0,5), pady=5)
+    return widget_holder
+
+def OpenFiles(form):
+    file = filedialog.askopenfilename(title='Elegí archivos .PDF', filetypes=(('Archivos PDF', '*.pdf'),('Todos los archivos', '*.*')))
+    reader = PdfReader(file)
+    content = []
+
+    for page in reader.pages:
+        for item in page.extract_text().splitlines():
+            content.append(item.strip())
+
+    name = content.index('Nombre del Producto:') + 1
+    id = content.index('D SBA:') + 1
+    sex = content.index('SEXO:') + 1
+    birth = content.index('Fecha de Nacimiento:') + 2
+    hair = birth - 3
+    tag = content.index('Tatuaje:') + 1
+    establishment = content.index('Criador:') + 1
+    owner = content.index('Propietario:') + 1
+
+    viewed_horse = [{
+        'Nombre' : content[name],
+        'SBA' : content[id]
+        },
+        {
+        'Sexo' : content[sex],
+        'Pelaje' : content[hair],
+        'Nacimiento' : content[birth],
+        'RP' : content[tag]
+        },
+        {
+        'Criador' : content[establishment],
+        'Propietario' : content[owner]
+        }]
+
+    regex = 'D|{}|{}|{}'.format(viewed_horse[0]['SBA'], viewed_horse[1]['RP'], viewed_horse[1]['Pelaje'])
+    father_name = content[content.index(regex) - 7]
+    father_info = content[content.index(regex) - 6]
+    mother_name = content[content.index(regex) - 5]
+    mother_info = content[content.index(regex) - 4]
+
+    father_object = construct_progenitor_object('Padre', father_name, father_info)
+    mother_object = construct_progenitor_object('Madre', mother_name, mother_info)
+    form_matrix = [*viewed_horse, father_object, mother_object]
+    update_form(form, form_matrix)
+
+def construct_progenitor_object(sex, name, info):
+    info = info.split('|')
+    id = info[1]
+
+    info = info[2].split(' ', 1)
+    hair = info[1]
+
+    info = info[0].split(':')
+    tag = info[1]
+
+    progenitor_object = {
+        sex : name,
+        #'Pelaje' : hair,
+        'RP' : tag,
+        'SBA' : id
+    }
+    return progenitor_object
+
+def update_form(form, matrix):
+    for i, row in enumerate(form):
+        segment = matrix[i]
+        for key, value in row.items():
+            value[1].insert(0, segment[key])
+
+root = tk.Tk()
+app_style = ttk.Style()
+app_style.theme_use('alt')         #alt clam classic default 
+
+root.geometry('600x246')
+root.title('Criollos')
+#root.iconbitmap()
+#root.resizable(False, False)
+root.option_add('*tearOff', 'false')
+
+
+frame = ttk.Frame(root)
+frame.pack(fill='both')
+
+form = create_form(frame)
+
+file_button = ttk.Button(frame, text= 'Abrir', command= lambda: OpenFiles(form))
+file_button.pack(side='top', pady=(0,10))
+
+
+root.mainloop()
