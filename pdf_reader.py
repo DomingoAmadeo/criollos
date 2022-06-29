c431b27e53a7075cf7864c04c376e99274a6b862
Amadeo <domingofal@gmail.com>
2022-06-29 09:56:03 -0300

    Added commentary regarding data parser and vis

--- pdf_reader.py
+++ pdf_reader.py
@@ -5,6 +5,8 @@ from PyPDF2 import PdfReader
 
 
 def create_form(parent):
+
+    # .pack() method doesn't support grid placement, i'm creating a matrix to bypass that
     first_row = {
     'Nombre' : [None, None],
     'SBA' : [None, None, 10]
@@ -29,38 +31,41 @@ def create_form(parent):
     'RP' : [None, None, 6],
     'SBA' : [None, None, 10]
 }
-    widget_holder = [first_row, second_row, third_row, fourth_row, fifth_row]
+    widget_holder = [first_row, second_row, third_row, fourth_row, fifth_row]   # Matrix for grid placement
 
     for row in widget_holder:
-        new_frame = ttk.Frame(parent)
+        new_frame = ttk.Frame(parent)                                       # Frame for row packing
         new_frame.pack(fill='x', padx=5, pady=5)
-        for index, (key, value) in enumerate(row.items()):
-            value[0] = ttk.Label(new_frame, text= key + ' : ')
+        for index, (key, value) in enumerate(row.items()):                  # Label-Entry pairs assigned to the key with the label text string
+            value[0] = ttk.Label(new_frame, text= key + ' : ')                  
             if index == 0:
-                value[0]['width'] = 9
-            value[1] = ttk.Entry(new_frame)
+                value[0]['width'] = 9                                       # First entry box of each row alignment
             value[0].pack(side='left', padx= (5,0), pady=5)
-            if len(value) == 3:
-                value[1]['width'] = value[2]
-                value[1]['justify'] = 'right'
+            value[1] = ttk.Entry(new_frame)
+            if len(value) == 3:                                             # Using a hardcoded third value included in some of the lists
+                value[1]['width'] = value[2]                                # to assign width
+                value[1]['justify'] = 'right'                               # Numbers are justified right
                 if key == 'Sexo':
-                    value[1]['justify'] = 'left'
+                    value[1]['justify'] = 'left'                            # Text left
                 elif key == 'Nacimiento':
-                    value[1]['justify'] = 'center'
+                    value[1]['justify'] = 'center'                          # DoB is centered
                 value[1].pack(side='left', padx= (0,5), pady=5)
             else:
-                value[1].pack(side='left', expand= 1, fill='x', padx= (0,5), pady=5)
+                value[1].pack(side='left', expand= 1, fill='x', padx= (0,5), pady=5)    # Entries with long strings auto expand
     return widget_holder
 
 def OpenFiles(form):
+    # File selection
     file = filedialog.askopenfilename(title='Elegí archivos .PDF', filetypes=(('Archivos PDF', '*.pdf'),('Todos los archivos', '*.*')))
     reader = PdfReader(file)
     content = []
 
+    #File parsing and simplifying 
     for page in reader.pages:
         for item in page.extract_text().splitlines():
             content.append(item.strip())
 
+    #Content matching for data
     name = content.index('Nombre del Producto:') + 1
     id = content.index('D SBA:') + 1
     sex = content.index('SEXO:') + 1
@@ -70,6 +75,7 @@ def OpenFiles(form):
     establishment = content.index('Criador:') + 1
     owner = content.index('Propietario:') + 1
 
+    # Matrix replication and data insertion
     viewed_horse = [{
         'Nombre' : content[name],
         'SBA' : content[id]
@@ -93,10 +99,13 @@ def OpenFiles(form):
 
     father_object = construct_progenitor_object('Padre', father_name, father_info)
     mother_object = construct_progenitor_object('Madre', mother_name, mother_info)
-    form_matrix = [*viewed_horse, father_object, mother_object]
+
+    form_matrix = [*viewed_horse, father_object, mother_object] # Matrix format replicated
+
     update_form(form, form_matrix)
 
 def construct_progenitor_object(sex, name, info):
+    #Separating grouped data
     info = info.split('|')
     id = info[1]
 
@@ -115,6 +124,7 @@ def construct_progenitor_object(sex, name, info):
     return progenitor_object
 
 def update_form(form, matrix):
+    #Overlapping widget matrix with data matrix to update fields
     for i, row in enumerate(form):
         segment = matrix[i]
         for key, value in row.items():
@@ -122,7 +132,7 @@ def update_form(form, matrix):
 
 root = tk.Tk()
 app_style = ttk.Style()
-app_style.theme_use('alt')         #alt clam classic default 
+app_style.theme_use('alt')          # alt clam classic default 
 
 root.geometry('600x246')
 root.title('Criollos')
@@ -134,7 +144,7 @@ root.option_add('*tearOff', 'false')
 frame = ttk.Frame(root)
 frame.pack(fill='both')
 
-form = create_form(frame)
+form = create_form(frame)           # PDF widget visualization gets tracked
 
 file_button = ttk.Button(frame, text= 'Abrir', command= lambda: OpenFiles(form))
 file_button.pack(side='top', pady=(0,10))
